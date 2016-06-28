00dd3f4e45f2973d26608922560f5e5481927f28
silivizzle <tiberius220@gmail.com>
2016-06-28 07:23:22 -0500

    added the references to the language specific package modules

---
+++ environments/test/manifests/package_init.pp
@@ -0,0 +1,11 @@
+
+class packages{
+
+    include test::packages::base_packages.pp
+    include test::packages::go_packages.pp
+    include test::packages::java_packages.pp
+    include test::packages::php_packages.pp
+    include test::packages::python_packages.pp
+    include test::packages::ruby_packages.pp
+
+}
