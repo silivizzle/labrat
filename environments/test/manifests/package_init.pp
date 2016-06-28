517e9cd33f89ca8a52d9d21d1090e6d482982589
silivizzle <tiberius220@gmail.com>
2016-06-28 07:24:16 -0500

    added a module to install node packages with

--- environments/test/manifests/package_init.pp
+++ environments/test/manifests/package_init.pp
@@ -4,6 +4,7 @@ class packages{
     include test::packages::base_packages.pp
     include test::packages::go_packages.pp
     include test::packages::java_packages.pp
+    include test::packages::node_packages.pp
     include test::packages::php_packages.pp
     include test::packages::python_packages.pp
     include test::packages::ruby_packages.pp
