70b9764c64dfb0e003d77fdbc6f20fc908fc3884
silivizzle <tiberius220@gmail.com>
2016-07-06 18:06:44 -0500

    code cleanup

--- labrat_install.sh
+++ labrat_install.sh
@@ -6,4 +6,4 @@ vagrant box add bento/centos-7.1                \
 vagrant box add bento/centos-6.7                \
 vagrant box add antoniomeireles/coreos-stable   \
 vagrant box add ubuntu/trusty64                 \
-vagrant box add ubuntu/xenial64                 \
+vagrant box add ubuntu/xenial64
