edfa260ffee4c5526884f8195538921c7dc99072
silivizzle <tiberius220@gmail.com>
2016-07-21 11:31:03 -0500

    added the provider to the install script

--- labrat_install.sh
+++ labrat_install.sh
@@ -2,8 +2,8 @@
 
 cd ~ \
 
-vagrant box add bento/centos-7.1                ;
-vagrant box add bento/centos-6.7                ;
-vagrant box add antoniomeireles/coreos-stable   ;
-vagrant box add ubuntu/trusty64                 ;
-vagrant box add ubuntu/xenial64
+vagrant box add bento/centos-7.1 --provider virtualbox                  ;
+vagrant box add bento/centos-6.7 --provider virtualbox                  ;
+vagrant box add antoniomeireles/coreos-stable --provider virtualbox     ;
+vagrant box add ubuntu/trusty64 --provider virtualbox                   ;
+vagrant box add ubuntu/xenial64 --provider virtualbox
